d02255696a47a0e7541159dce632d85e1c69140d
afuaarmah <afuaarmah@gmail.com>
2025-10-28 16:04:56 +0100

    Import Link from react-router-dom to fix CI build

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-//import { Link, useNavigate } from 'react-router-dom';//
+//import { useNavigate } from 'react-router-dom';//
+import { Link } from 'react-router-dom';
 //import apiService from '../services/api';//
 import './Auth.css';
 
